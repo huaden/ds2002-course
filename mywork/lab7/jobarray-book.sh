16d1601606ef0538af6f02508e0beae616256677
huaden <xyb9vz@virginia.edu>
2026-03-18 15:17:10 -0400

    Complete Lab 07: HPC job arrays

---
+++ mywork/lab7/jobarray-book.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=ds2002
+#SBATCH --job-name=ds2002_lab7_part3
+#SBATCH --output=jobarray-book-%A-%a.out
+#SBATCH --error=jobarray-book-%A-%a.err
+#SBATCH --time=00:10:00
+#SBATCH --partition=standard
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-5
+
+
+INPUT="book-${SLURM_ARRAY_TASK_ID}.txt"
+OUTPUT="results-${SLURM_ARRAY_TASK_ID}.csv"
+
+python ~/ds2002-course/labs/07-hpc/process-book.py "$INPUT" "$OUTPUT"
